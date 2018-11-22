d101a89ca72633ae3ac96b40d4fa02b69c6f627d
Samuel Pulfer <samuel.pulfer@students.bfh.ch>
2018-11-22 17:13:27 +0100

    Packages, and JNDB prepared

--- AddictoMed/src/main/java/ch/bfh/btx8081/w2018/black/view/routes/MainView.java
+++ AddictoMed/src/main/java/ch/bfh/btx8081/w2018/black/view/routes/MainView.java
@@ -1,4 +1,4 @@
-package ch.bfh.btx8081.w2018;
+package ch.bfh.btx8081.w2018.black.view.routes;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.notification.Notification;
@@ -7,11 +7,21 @@ import com.vaadin.flow.router.Route;
 
 /**
  * The main view contains a button and a click listener.
+ * @author Unknown
  */
+
 @Route("")
 public class MainView extends VerticalLayout {
 
-    public MainView() {
+    /**
+	 * Generated Serial
+	 */
+	private static final long serialVersionUID = 1743481046132108606L;
+
+	/**
+	 * Shows main view containing a button
+	 */
+	public MainView() {
         Button button = new Button("Click me",
                 event -> Notification.show("Clicked!"));
         add(button);
