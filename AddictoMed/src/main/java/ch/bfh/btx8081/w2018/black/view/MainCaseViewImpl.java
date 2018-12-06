b3ddc957a878773968fc05ec212878bd8b677053
Samuel Pulfer <samuel.pulfer@students.bfh.ch>
2018-12-06 11:21:13 +0100

    formatting

--- AddictoMed/src/main/java/ch/bfh/btx8081/w2018/black/view/MainCaseViewImpl.java
+++ AddictoMed/src/main/java/ch/bfh/btx8081/w2018/black/view/MainCaseViewImpl.java
@@ -4,70 +4,62 @@ import java.util.List;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.datepicker.DatePicker;
-import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.data.binder.HasDataProvider;
-
-import ch.bfh.btx8081.w2018.black.model.MainCasesModelImpl;
-import ch.bfh.btx8081.w2018.black.model.ifaces.MainCasesModel;
 import ch.bfh.btx8081.w2018.black.model.ifaces.MainCasesModel.Case;
 import ch.bfh.btx8081.w2018.black.view.ifaces.MainCaseView;
 
-
 public class MainCaseViewImpl extends VerticalLayout implements MainCaseView {
 
-	
-	 Tabs tabs = new Tabs();
-	  
-	  TextField insuranceNr = new TextField();
-	  TextField place = new TextField();
-	  DatePicker startDate = new DatePicker();
-	  DatePicker endDate = new DatePicker();
-	  Button add = new Button("Add");
-	  
-	  
-	  public MainCaseViewImpl() {
-	    enableEdit(false);
-	    startDate.setLabel("Start Date");
-	    endDate.setLabel("End Date");
-	    insuranceNr.setLabel("InsuranceNr.");
-	    place.setLabel("Place");
-	    tabs.add(new Tab(add));
-	    HorizontalLayout caseInfos = new HorizontalLayout(startDate, endDate, place, insuranceNr);
-	    tabs.addSelectedChangeListener(event -> {
-	      // inform each listener
-	      // Integer.valueOf(tabs.getSelectedTab().getLabel());
-	    });
-	    add.addClickListener(event -> {
-	      // inform each listener
-	    });
-	    add(tabs, caseInfos);
-	  }
+	Tabs tabs = new Tabs();
 
-	  @Override
-	  public void setCases(List<Case> cases) {
-	    tabs.removeAll();
-	    tabs.add(new Tab(add));
-	    for(Case vcase:cases) {
-	      tabs.add(new Tab(String.valueOf(vcase.getCaseID())));
-	    }
-	  }
-	  
-	  public void setCase(Case vcase) {
-	    insuranceNr.setValue(String.valueOf(vcase.getInsuranceNumber()));
-	    place.setValue(vcase.getPlace());
-	    startDate.setValue(vcase.getStartDate());
-	    endDate.setValue(vcase.getEndDate());
-	  }
-	  
-	  public void enableEdit(Boolean edit) {
-	    insuranceNr.setEnabled(edit);
-	    place.setEnabled(edit);
-	    startDate.setEnabled(edit);
-	    endDate.setEnabled(edit);
-	  }		
+	TextField insuranceNr = new TextField();
+	TextField place = new TextField();
+	DatePicker startDate = new DatePicker();
+	DatePicker endDate = new DatePicker();
+	Button add = new Button("Add");
+
+	public MainCaseViewImpl() {
+		enableEdit(false);
+		startDate.setLabel("Start Date");
+		endDate.setLabel("End Date");
+		insuranceNr.setLabel("InsuranceNr.");
+		place.setLabel("Place");
+		tabs.add(new Tab(add));
+		HorizontalLayout caseInfos = new HorizontalLayout(startDate, endDate, place, insuranceNr);
+		tabs.addSelectedChangeListener(event -> {
+			// inform each listener
+			// Integer.valueOf(tabs.getSelectedTab().getLabel());
+		});
+		add.addClickListener(event -> {
+			// inform each listener
+		});
+		add(tabs, caseInfos);
+	}
+
+	@Override
+	public void setCases(List<Case> cases) {
+		tabs.removeAll();
+		tabs.add(new Tab(add));
+		for (Case vcase : cases) {
+			tabs.add(new Tab(String.valueOf(vcase.getCaseID())));
+		}
+	}
+
+	public void setCase(Case vcase) {
+		insuranceNr.setValue(String.valueOf(vcase.getInsuranceNumber()));
+		place.setValue(vcase.getPlace());
+		startDate.setValue(vcase.getStartDate());
+		endDate.setValue(vcase.getEndDate());
+	}
+
+	public void enableEdit(Boolean edit) {
+		insuranceNr.setEnabled(edit);
+		place.setEnabled(edit);
+		startDate.setEnabled(edit);
+		endDate.setEnabled(edit);
+	}
 }
