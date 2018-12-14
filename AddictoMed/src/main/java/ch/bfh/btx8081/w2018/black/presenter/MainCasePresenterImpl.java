c2c7f311cec8a196872441cb0cb0954a3cf48812
Michael Däppen <michael.daeppen@gmail.com>
2018-12-14 10:59:20 +0100

    added LabResults Model

--- AddictoMed/src/main/java/ch/bfh/btx8081/w2018/black/presenter/MainCasePresenterImpl.java
+++ AddictoMed/src/main/java/ch/bfh/btx8081/w2018/black/presenter/MainCasePresenterImpl.java
@@ -3,6 +3,7 @@ package ch.bfh.btx8081.w2018.black.presenter;
 import java.util.List;
 
 import ch.bfh.btx8081.w2018.black.model.ifaces.MainCasesModel;
+import ch.bfh.btx8081.w2018.black.model.ifaces.MainCasesModel.Case;
 import ch.bfh.btx8081.w2018.black.view.ifaces.MainCaseView;
 
 public class MainCasePresenterImpl {
@@ -14,5 +15,10 @@ public class MainCasePresenterImpl {
 		this.view = view;
 		this.model = model;
 	}
+	
+//	public void setCases() {
+//		List<Case> cases = model.getCaseList();
+//		view.setCases(cases);
+//	}
 	
 }
