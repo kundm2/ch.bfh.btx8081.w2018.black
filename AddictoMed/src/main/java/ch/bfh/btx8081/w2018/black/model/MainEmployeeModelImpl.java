43da4944b6cd914e4afb20821dd671f40d52c8d0
Samuel Pulfer <samuel.pulfer@students.bfh.ch>
2018-12-14 22:32:11 +0100

    classes created

---
+++ AddictoMed/src/main/java/ch/bfh/btx8081/w2018/black/model/MainEmployeeModelImpl.java
@@ -0,0 +1,21 @@
+package ch.bfh.btx8081.w2018.black.model;
+
+import java.util.List;
+
+import ch.bfh.btx8081.w2018.black.model.ifaces.MainEmployeeModel;
+
+public class MainEmployeeModelImpl implements MainEmployeeModel {
+
+	@Override
+	public List<Employee> getEmployees() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Employee getEmployeeById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
