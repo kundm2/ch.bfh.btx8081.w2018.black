7ffe7e37725ffca6fd9402601564dfaad6147f37
Moritz <35421707+kundm2@users.noreply.github.com>
2019-01-16 21:51:26 +0100

    Update patient report

--- AddictoMed/src/main/java/ch/bfh/btx8081/w2018/black/view/PatientReportViewImpl.java
+++ AddictoMed/src/main/java/ch/bfh/btx8081/w2018/black/view/PatientReportViewImpl.java
@@ -7,7 +7,6 @@ import java.util.List;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Anchor;
-import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.server.StreamResource;
@@ -17,15 +16,16 @@ import ch.bfh.btx8081.w2018.black.view.ifaces.PatientReportView;
 public class PatientReportViewImpl extends VerticalLayout implements PatientReportView {
 	private List<PatientReportGenerateListener> listeners = new ArrayList<>();
 	private TextField patientId = new TextField("Patientennummer");
+	private TextField error = new TextField("Error");
 	private Button generate = new Button("Generieren");
 	private Anchor downloadlink = new Anchor();
 
 	public PatientReportViewImpl() {
+		error.setVisible(false);
 		generate.addClickListener(event -> {
 			Integer patIdInt = null;
 			try {
 				patIdInt = Integer.valueOf(patientId.getValue());
-				Notification.show(patIdInt.toString());
 				for (PatientReportGenerateListener listener : listeners) {
 					listener.generateReport(patIdInt);
 				}
@@ -33,7 +33,7 @@ public class PatientReportViewImpl extends VerticalLayout implements PatientRepo
 
 			}
 		});
-		add(patientId, generate, downloadlink);
+		add(patientId, generate, error, downloadlink);
 	}
 
 	@Override
@@ -48,5 +48,11 @@ public class PatientReportViewImpl extends VerticalLayout implements PatientRepo
 	public void addPatientReportGenerateListener(PatientReportGenerateListener listener) {
 		listeners.add(listener);
 
+	}
+
+	@Override
+	public void setError(String error) {
+		this.error.setValue(error);
+		this.error.setVisible(true);
 	}
 }
