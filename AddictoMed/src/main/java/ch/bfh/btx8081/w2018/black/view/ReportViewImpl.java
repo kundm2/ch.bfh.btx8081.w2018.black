dba757cc9b50c33c0d4f276fc6c78afe0cadfa11
armon.dressler <armon.dressler@gmail.com>
2019-01-17 17:54:22 +0100

    fix für merge konflikt

--- AddictoMed/src/main/java/ch/bfh/btx8081/w2018/black/view/ReportViewImpl.java
+++ AddictoMed/src/main/java/ch/bfh/btx8081/w2018/black/view/ReportViewImpl.java
@@ -10,6 +10,7 @@ import com.vaadin.flow.router.RouterLink;
 
 import ch.bfh.btx8081.w2018.black.view.routes.BusinessReport;
 import ch.bfh.btx8081.w2018.black.view.routes.WeeklyReport;
+import ch.bfh.btx8081.w2018.black.view.routes.DailyReport;
 
 public class ReportViewImpl extends VerticalLayout {
 
@@ -18,20 +19,14 @@ public class ReportViewImpl extends VerticalLayout {
 	 */
 	private static final long serialVersionUID = -2878625525322252003L;
 
-	Button btnGenDailyReport = new Button("Tagesreport");
-
-	DatePicker datePickerDailyReport = new DatePicker();
-
-	HorizontalLayout layoutDailyReport = new HorizontalLayout();
-
 	public ReportViewImpl() {
-
-		layoutDailyReport.add(datePickerDailyReport, btnGenDailyReport);
 
 		//RouterLink caseReport = new RouterLink(null, CaseReport.class);
 		//caseReport.add(new Button("Fallreport", new Icon(VaadinIcon.CLIPBOARD_TEXT)));
 
-		add(layoutDailyReport);
+
+		RouterLink dailyReport = new RouterLink(null, DailyReport.class);
+		dailyReport.add(new Button("Tagesreport", new Icon(VaadinIcon.CLIPBOARD_PULSE)));	
 
 		RouterLink businessReport = new RouterLink(null, BusinessReport.class);
 		businessReport.add(new Button("Business Report", new Icon(VaadinIcon.CLIPBOARD_PULSE)));
@@ -42,7 +37,7 @@ public class ReportViewImpl extends VerticalLayout {
 		RouterLink weeklyReport = new RouterLink(null, WeeklyReport.class);
 		weeklyReport.add(new Button("Wochenreport", new Icon(VaadinIcon.CLIPBOARD_CHECK)));
 		
-		add(layoutDailyReport, weeklyReport, businessReport);
+		add(dailyReport, weeklyReport, businessReport);
 
 		// btnGenWeeklyReport.addClickListener(e-> {btnGenWeeklyReport.getUI().ifPresent(ui -> ui.navigate("WeeklyReport"));});
 	}
